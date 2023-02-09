eb8e01effc0342a2f1cc3c92bc75fb77c5030860
Joshua Jumelet <joshuajumelet@gmail.com>
2023-02-09 09:48:42 -0800

    model recipe.js schema file created

--- models/recipe.js
+++ models/recipe.js
@@ -1,1 +1,14 @@
+const { Schema } = require('mongoose')
 
+const Recipe = new Schema(
+  {
+    title: { type: String, required: true },
+    ingredients: { type: String, required: true },
+    instructions: { type: String, required: true },
+    image: { type: String, required: true },
+    comment_id: { type: Schema.Types.ObjectId, ref: 'comment_id' }
+  },
+  { timestamps: true }
+)
+
+mondule.exports = Recipe
