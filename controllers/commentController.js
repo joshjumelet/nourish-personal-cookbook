7d61a3e6e8600876939bb5a091be3d77f788275e
Joshua Jumelet <joshuajumelet@gmail.com>
2023-02-10 13:21:35 -0800

    complete building comment controller & routes

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -12,6 +12,44 @@ const createComment = async (req, res) => {
   }
 }
 
+const getRecipeComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({ recipe: req.params.id })
+    return res.status(200).json(comments)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const udpateComment = async (req, res) => {
+  try {
+    const commentUpdate = await Comment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(commentUpdate)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const deleteComment = async (req, res) => {
+  try {
+    const { id } = req.params
+    const commentDelete = await Comment.findByIdAndDelete(id)
+    if (commentDelete) {
+      return res.status(200).send('Comment has been deleted.')
+    }
+    throw new Error('Comment not found.')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createComment
+  createComment,
+  getRecipeComments,
+  udpateComment,
+  deleteComment
 }
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = Router()
 const recipeController = require('../controllers/recipeController.js')
 const commentController = require('../controllers/commentController.js')
 
+/// Homepage Test ///
 router.get('/', (req, res) => res.send('Welcome to the Homepage!'))
 
+/// Recipe Routes ///
 router.post('/recipes', recipeController.createRecipe)
 
 router.get('/recipes', recipeController.getAllRecipes)
@@ -15,6 +17,13 @@ router.put('/recipes/:id', recipeController.updateRecipe)
 
 router.delete('/recipes/:id', recipeController.deleteRecipe)
 
+/// Comment Routes ///
 router.get('/comments', commentController.createComment)
+
+router.get('/comments/:id', commentController.getRecipeComments)
+
+router.put('/comments/:id', commentController.udpateComment)
+
+router.delete('/comments/:id', commentController.deleteComment)
 
 module.exports = router
