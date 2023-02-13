1d76e93c34ab2c0932bb1a9a78914af496d42d37
Joshua Jumelet <joshuajumelet@gmail.com>
2023-02-13 09:25:52 -0800

    create Home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,12 @@ const Home = () => {
   
   return (
     <div className="home">
-      <div></div>
+      <div>
+        <h1>Nourish</h1>
+        <h2>A Personal Cookbook</h2>
+      </div>
     </div>
   )
 }
+
+export default Home
